c995803d5eb4b37ad234dd1b57d5026bba4473c4
baiamoraes <85076898+baiamoraes@users.noreply.github.com>
2024-12-05 16:23:24 -0300

    Update App.js

--- Atividade03/cotacao-moedas/App.js
+++ Atividade03/cotacao-moedas/App.js
@@ -1,1 +1,19 @@
-teste
+import 'react-native-gesture-handler';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import Dolar from './src/screens/Dolar'; 
+import Euro from './src/screens/Euro';  
+
+const Drawer = createDrawerNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Dólar">
+        <Drawer.Screen name="Dólar Americano" component={Dolar} />
+        <Drawer.Screen name="Euro" component={Euro} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
